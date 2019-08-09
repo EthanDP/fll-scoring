351de81e85077a016138cb75ead3af8303cf16ec
ECreates <etdpeck@gmail.com>
2019-08-08 20:42:26 -0500

    Basic model layout, migrations

--- site_resources/scoring_system/admin.py
+++ site_resources/scoring_system/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import EventUser, ScoringCategory, ScoringCriteria
 
-# Register your models here.
+admin.site.register(EventUser)
+admin.site.register(ScoringCategory)
+admin.site.register(ScoringCriteria)
+
+
--- site_resources/scoring_system/models.py
+++ site_resources/scoring_system/models.py
@@ -1,4 +1,25 @@
 from django.db import models
 
-class User:
-    pass
+class EventUser(models.Model):
+    # Account data for event scorekeepers
+    username = models.CharField(max_length=20)
+
+class Team(models.Model):
+    # Team data, will be available for event scorekeeprs to modify
+    name = models.CharField(max_length=50)
+    number = models.PositiveSmallIntegerField()
+    points = models.PositiveSmallIntegerField()
+
+class ScoringCriteria(models.Model):
+    # The individual tasks in a scoring category, can only be modified by 
+    point_value = models.SmallIntegerField()
+    sub_category = models.CharField(max_length=10) # If a criteria is boolean based (yes or no) and shares
+                                      # a sub_category with another criteria they cannot both
+                                      # be checked yes/true at the same time
+
+
+class ScoringCategory(models.Model):
+    # An individual category containing one or more scoring criteria
+    category_name = models.CharField(max_length=50)
+    category_image = models.FileField()
+    category_criteria = models.ManyToManyField(ScoringCriteria, symmetrical=False)
