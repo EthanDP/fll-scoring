42cd9c334f1b9a76667a19767a9a90099bbb1f6e
Ethan Peck <etdpeck@gmail.com>
2019-10-18 08:21:46 -0500

    Channels setup

--- site_resources/scoring_site/routing.py
+++ site_resources/scoring_site/routing.py
@@ -1,5 +1,11 @@
-from channels.routing import ProtocolTypeRouter
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+import scoring_system.routing
 
 application = ProtocolTypeRouter({
-    
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            scoring_system.routing.websocket_urlpatterns
+        )
+    ),
 })
---
+++ site_resources/scoring_system/consumers.py
@@ -0,0 +1,4 @@
+import asyncio
+import json
+from channels.generic.websocket import WebsocketConsumer
+from channels.consumer import AsyncConsumer
